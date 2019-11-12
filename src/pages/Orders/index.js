e94fe69ceb9b85f54fe79dc1d5ba03abca5fdc55
Arnaldo Victor <arnaldovictorm@gmail.com>
2019-11-11 23:12:28 -0300

    Tela Pedidos criado

---
+++ src/pages/Orders/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+
+export default (props) => {
+  return (
+    <View />
+  );
+}
---
+++ src/pages/Orders/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Dimensions } from 'react-native';
+const width = Dimensions.get('screen').width;
+
+const styles = StyleSheet.create({
+
+
+
+})
+
+export default styles;
